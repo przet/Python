c843bf43077008a25d413d27e90289210b343930
CrazyPenguin <p.rzetelski.note@gmail.com>
2018-07-23 17:00:58 +0930

    Hello world and starting on variables and expressions

---
+++ variablesAndExpression.py
@@ -0,0 +1,12 @@
+# Declare a variable and initialize it
+f = 0
+print(f)
+f = "abc"
+print(f)
+# redeclaring the variable works.
+
+#However, variables of different types cannot be combined
+#print("This is a string" + 12) #won't work! Note it WILL work in JS.
+#Python is strongly typed, JS is weakly typed
+
+
