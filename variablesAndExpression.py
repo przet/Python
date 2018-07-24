a0a65db73b8c0d0dd21370ad842983b7b5c73cef
CrazyPenguin <p.rzetelski.note@gmail.com>
2018-07-24 17:17:23 +0930

    More variables and expressions

--- variablesAndExpression.py
+++ variablesAndExpression.py
@@ -9,4 +9,21 @@ print(f)
 #print("This is a string" + 12) #won't work! Note it WILL work in JS.
 #Python is strongly typed, JS is weakly typed
 
+#Global vs local variables in functions
+
+def someFunction():
+    f = "def"
+    print(f)
+
+someFunction() # will print "def" - function has own local copy
+print(f) # will be "abc"
+
+def someFunction2():
+    global f
+    f = "def"
+    print(f)
+
+someFunction2() # "def"
+print(f) # "def"
+
 
